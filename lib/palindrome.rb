05061c28f60213712d760e3bb3ddeaa960b7a4ae
Raymond Gan <rayning@yahoo.com>
2015-12-15 01:39:55 -0800

    Solve array copy problem by not 'dup' arr. Instead, create new 'arr' (with NEW MEMORY ADDRESS).

--- lib/palindrome.rb
+++ lib/palindrome.rb
@@ -1,12 +1,6 @@
 class Palindrome
   def find_pal(str)
     s = str.split(',')
-
-    # iterate thru array
-    # [1, 7] =? [7, 1] NO
-    # [1, 7, 3] =? [3, 7, 1] NO
-    # [1, 7...2] =? [2...1] NO
-    # [7, 3] =? [3, 7] NO
     longest_pal = []
 
     s.each_with_index do |num_start, index|
@@ -14,23 +8,19 @@ class Palindrome
 
       s[index + 1..-1].each do |num_end|
 
-        # make COPY of arr with "dup" (different memory address)
-        array_copy = arr.dup << num_end
-
-        arr << num_end
+  # Don't do arr << num_end, because this
+  # does NOT change memory address of arr!
+  # arr = arr + [num_end] really means:
+  # arr (NEW MEMORY ADDRESS) = arr (OLD MEMORY ADDRESS) + [num_end]
+        arr = arr + [num_end]
         p arr
         if is_palindrome?(arr) && arr.length > longest_pal.length
           puts "arr = #{arr}, longest_pal=#{longest_pal}"
 
-    # Don't put longest_pal = arr, because then longest_pal
-    # points to MEMORY ADDRESS of arr. As arr increases size,
-    # longest_pal will too. In other words, saying "a = b" in Ruby
-    # means "Point a to home address of b. No matter what b actually
-    # equals, a will equal the same."
-
-    # Setting longest_pal = a COPY of arr means longest_pal will
-    # NOT automatically change when arr changes!
-          longest_pal = array_copy
+          # points longest_pal to NEW MEMORY ADDRESS of arr,
+          # keeping longest_pal from changing when
+          # arr (OLD MEMORY ADDRESS) does!
+          longest_pal = arr
         end
       end
 
